4989992271b75dc001365c0c2df0956d158f8c4d
haoshengqiang <haoshengqiang@xiaohongshu.com>
2022-05-22 20:05:37 +0800

    update

---
+++ common/onnx2trt.py
@@ -0,0 +1,108 @@
+#! /usr/bin/env python3
+
+import tensorrt as trt
+import torch
+import numpy as np
+import onnx
+from onnx import helper
+from onnx import AttributeProto, TensorProto, GraphProto
+import polygraphy
+import polygraphy.backend
+import polygraphy.backend.onnx
+import polygraphy.constants
+
+
+def covert2trt(model: onnx.ModelProto,profile_shapes ,fp16 ,engine_plan_path):
+    logger = trt.Logger(trt.Logger.VERBOSE)
+    builder = trt.Builder(logger)
+    network = builder.create_network(
+        (1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)) | (1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_PRECISION)))
+
+
+    parser = trt.OnnxParser(network, logger)
+    success = parser.parse(model)
+
+    for idx in range(parser.num_errors):
+        print(parser.get_error(idx))
+    if not success:
+        print("parse failed!")
+        exit()
+
+    fp32_layer_list = []
+    i = 0
+    for i in range(network.num_layers):
+        layer = network[i]
+        print("idx:{0};name: {1}; type: {2};num_inputs: {3};num_outputs: {4};precision: {5};precision_is_set: {6}".format(
+            i, layer.name, layer.type, layer.num_inputs, layer.num_outputs, layer.precision, layer.precision_is_set))
+        if layer.type != trt.LayerType.SCALE and layer.type != trt.LayerType.RESIZE and layer.type != trt.LayerType.SELECT and layer.type != trt.LayerType.SHAPE and layer.type != trt.LayerType.SLICE and layer.type != trt.LayerType.ELEMENTWISE and (not layer.precision_is_set) and layer.type != trt.LayerType.CONSTANT and layer.type != trt.LayerType.SHUFFLE and layer.type != trt.LayerType.CONCATENATION and layer.type != trt.LayerType.GATHER and layer.type != trt.LayerType.IDENTITY:
+            fp32_layer_list.append(i)
+
+    print("len: {0}, idx: {1}".format(len(fp32_layer_list), fp32_layer_list))
+
+
+    def set_layer_precision(network, idx, precision):
+        if idx >= network.num_layers:
+            return
+        layer = network[idx]
+        if layer.precision_is_set:
+            print("**************************{} precision has been set".format(idx))
+            return
+        if layer.type == trt.LayerType.CONSTANT:
+            print("**************************{} constant layer".format(idx))
+            return
+        if layer.type == trt.LayerType.SHUFFLE:
+            print("**************************{} SHUFFLE layer".format(idx))
+            return
+        if layer.type == trt.LayerType.CONCATENATION:
+            print("**************************{} CONCATENATION layer".format(idx))
+            return
+        if layer.type == trt.LayerType.GATHER:
+            print("**************************{} GATHER layer".format(idx))
+            return
+        if layer.type == trt.LayerType.IDENTITY:
+            print("**************************{} IDENTITY layer".format(idx))
+            return
+        layer.reset_precision()
+        layer.precision = precision
+        for i in range(layer.num_outputs):
+            if layer.get_output_type(i) != trt.int32 and layer.get_output_type(i) != trt.bool:
+                layer.set_output_type(i, precision)
+
+
+    config = builder.create_builder_config()
+    config.max_workspace_size = 1 << 33
+    profile = builder.create_optimization_profile()
+    for i in range(len(profile_shapes)):
+        input_name = "INPUT__" + str(i)
+        min_shape = profile[input_name][0]
+        opt_shape = profile[input_name][1]
+        max_shape = profile[input_name][2]
+        profile.set_shape(input_name,min_shape,opt_shape,max_shape)
+
+    config.add_optimization_profile(profile)
+    if fp16:
+        config.set_flag(trt.BuilderFlag.FP16)
+    # config.set_flag(trt.BuilderFlag.OBEY_PRECISION_CONSTRAINTS)
+    config.set_flag(trt.BuilderFlag.DEBUG)
+    config.set_flag(trt.BuilderFlag.DIRECT_IO)
+    config.set_flag(trt.BuilderFlag.REJECT_EMPTY_ALGORITHMS)
+    # config.set_flag(trt.BuilderFlag.PREFER_PRECISION_CONSTRAINTS)
+
+
+    # def remove_items(a, b):
+    #     for idx in b:
+    #         if idx in a:
+    #             a.remove(idx)
+
+
+    # fp32_list = fp32_layer_list
+
+
+    # for idx in fp32_list:
+    #     print("{0} set precision".format(idx))
+    #     set_layer_precision(network, idx, trt.float32)
+
+
+    serialized_engine = builder.build_serialized_network(network, config)
+    with open(engine_plan_path, "wb") as f:
+        f.write(serialized_engine)
--- onnx-single-sanitize.py
+++ onnx-single-sanitize.py
@@ -4,6 +4,7 @@ from cgi import print_arguments
 import os
 import argparse
 import contextlib
+import profile
 import re
 import shlex
 import shutil
@@ -16,25 +17,29 @@ from onnx import shape_inference
 from onnx.tools import update_model_dims
 
 from common.fold_constatns import fold_constants
-from common.convert_to_str import value_infos_to_str,nodes_to_str
+from common.convert_to_str import value_infos_to_str, nodes_to_str
 from common.rename_input_output import rename_input_output
+from common.onnx2trt import covert2trt
+
 
 def make_parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input_onnx", required=True,
                         type=str, help="input onnx model")
-    parser.add_argument("-o", "--output_onnx", required=True,
-                        type=str, help="output onnx model")
+    parser.add_argument("-o", "--output_plan", required=True,
+                        type=str, help="output trt model")
+    parser.add_argument("--fp16", action='store_true', help="whether use fp16")
     return parser.parse_args()
 
-def main(model: onnx.ModelProto, print_info= True)-> onnx.ModelProto:
+
+def main(model: onnx.ModelProto, profile_shapes, fp16, engine_plan_path, print_info=True) -> onnx.ModelProto:
     decorate = "*"*15
-    if print_info: 
+    if print_info:
         print("before renamed model info")
         print("{0}input{1}\n{2}".format(decorate, decorate,
-            value_infos_to_str(model.graph.input)))
+                                        value_infos_to_str(model.graph.input)))
         print("{0}output{1}\n{2}".format(decorate, decorate,
-            value_infos_to_str(model.graph.output)))
+                                         value_infos_to_str(model.graph.output)))
     if (len(model.graph.input) != 1) or (len(model.graph.output) != 1):
         print("input/output size not 1")
 
@@ -44,10 +49,12 @@ def main(model: onnx.ModelProto, print_info= True)-> onnx.ModelProto:
     if print_info:
         print("\nafter renamed model info")
         print("{0}input{1}\n{2}".format(decorate, decorate,
-            value_infos_to_str(model.graph.input)))
+                                        value_infos_to_str(model.graph.input)))
         print("{0}output{1}\n{2}".format(decorate, decorate,
-            value_infos_to_str(model.graph.output)))
-    return fold_constants(model)
+                                         value_infos_to_str(model.graph.output)))
+    model = fold_constants(model)
+    covert2trt(model=model, fp16=fp16, profile_shapes=profile_shapes,
+               engine_plan_path=engine_plan_path)
 
     # model = update_model_dims.update_inputs_outputs_dims(model,{"INPUT__0":['b',3,640,640]},{"OUTPUT__0":['b',8400,19]})
 
@@ -56,6 +63,7 @@ if __name__ == "__main__":
     args = make_parse()
     input_model_path = args.input_onnx
     output_model_path = args.output_onnx
+    fp16 = args.fp16
+    profile_shapes = {"INPUT__0":[(1,3,640,640),(1,3,640,640),(1,3,640,640)]}
     model = onnx.load(input_model_path)
-    model = main(model)
-    onnx.save(model,output_model_path)
+    main(model=model,profile_shapes=profile_shapes,fp16=fp16,engine_plan_path=output_model_path)
